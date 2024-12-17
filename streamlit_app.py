9f5aeb9a025d202c41db27a298a916df9a8c024a
lars-fritz <159168398+lars-fritz@users.noreply.github.com>
2024-12-17 13:59:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import requests
 from dune_client.client import DuneClient
 from sklearn.linear_model import LinearRegression
 
